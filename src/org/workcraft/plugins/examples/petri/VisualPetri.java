a891ae30f4bca59ac52e54f7c892babe69e29ec1
Danil Sokolov <danilovesky@gmail.com>
2020-06-30 21:21:54 +0100

    Refactor model registration of graph edtor tools

--- src/org/workcraft/plugins/examples/petri/VisualPetri.java
+++ src/org/workcraft/plugins/examples/petri/VisualPetri.java
@@ -1,13 +1,13 @@
 package org.workcraft.plugins.examples.petri;
 
 import org.workcraft.annotations.DisplayName;
+import org.workcraft.dom.generators.DefaultNodeGenerator;
 import org.workcraft.dom.visual.AbstractVisualModel;
 import org.workcraft.dom.visual.VisualGroup;
-import org.workcraft.dom.generators.DefaultNodeGenerator;
-import org.workcraft.gui.tools.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.workcraft.gui.tools.CommentGeneratorTool;
+import org.workcraft.gui.tools.ConnectionTool;
+import org.workcraft.gui.tools.NodeGeneratorTool;
+import org.workcraft.gui.tools.SelectionTool;
 
 @DisplayName("Petri Net")
 public class VisualPetri extends AbstractVisualModel {
@@ -18,17 +18,15 @@ public class VisualPetri extends AbstractVisualModel {
 
     public VisualPetri(Petri model, VisualGroup root) {
         super(model, root);
-        setGraphEditorTools();
     }
 
-    private void setGraphEditorTools() {
-        List<GraphEditorTool> tools = new ArrayList<>();
-        tools.add(new SelectionTool());
-        tools.add(new CommentGeneratorTool());
-        tools.add(new ConnectionTool());
-        tools.add(new NodeGeneratorTool(new DefaultNodeGenerator(Place.class)));
-        tools.add(new NodeGeneratorTool(new DefaultNodeGenerator(Transition.class)));
-        setGraphEditorTools(tools);
+    @Override
+    public void registerGraphEditorTools() {
+        addGraphEditorTool(new SelectionTool());
+        addGraphEditorTool(new CommentGeneratorTool());
+        addGraphEditorTool(new ConnectionTool());
+        addGraphEditorTool(new NodeGeneratorTool(new DefaultNodeGenerator(Place.class)));
+        addGraphEditorTool(new NodeGeneratorTool(new DefaultNodeGenerator(Transition.class)));
     }
 
     @Override
